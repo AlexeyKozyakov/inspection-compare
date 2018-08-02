c324d8f491269ce7514157b417929e6636164414
AlexeyKozyakov <xelexerx@gmail.com>
2018-08-02 11:28:57 +0700

    filter-diff action

---
+++ src/plugin/FilterDiffAction.java
@@ -0,0 +1,14 @@
+package plugin;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class FilterDiffAction extends AnAction {
+    public FilterDiffAction() {
+        super(" Filter/diff inspection result");
+    }
+    @Override
+    public void actionPerformed(AnActionEvent event) {
+
+    }
+}
